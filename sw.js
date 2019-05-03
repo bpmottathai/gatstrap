8ccc3875473517f33f2d0cb37840eeb1a9e622ec
kaithapram <kaithapram123@gmail.com>
2019-05-03 23:03:00 +0530

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-bdaebe688363bae6c766.js"
+    "url": "webpack-runtime-e06e2e563ce98149bd12.js"
   },
   {
     "url": "app-d80b451d29a29fa41924.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "05a69d338e502d97c407ffabbf343b31"
+    "revision": "48795398e12382bea9eb0294feeca038"
   },
   {
     "url": "static/d/604/path---offline-plugin-app-shell-fallback-a-30-c5a-BawJvyh36KKFwbrWPg4a4aYuc8.json"
